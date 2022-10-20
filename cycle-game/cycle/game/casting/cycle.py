df728d23cfcc8d1fd0d125911344c3da92193175
robinsonbrennen <rob16073@byui.edu>
2022-10-20 14:40:15 -0600

    cycle code changes to layout

--- cycle-game/cycle/game/casting/cycle.py
+++ cycle-game/cycle/game/casting/cycle.py
@@ -15,10 +15,9 @@ class Cycle(Actor):
         return self._segments
 
     def move_next(self):
-        # move all segments
         for segment in self._segments:
             segment.move_next()
-        # update velocities
+            
         for i in range(len(self._segments) - 1, 0, -1):
             trailing = self._segments[i]
             previous = self._segments[i - 1]
